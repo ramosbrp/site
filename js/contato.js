4f06d12335771f45188f5452fd70b50d14d92259
Bruno Ramos Pires <61557867+ramosbrp@users.noreply.github.com>
2025-01-25 18:03:07 -0300

    Develop (#20)
    * Testes finais para roteamento da SPA
    
    * Convertendo pra SPA com javaScrit e node.js
    
    * Ajustes gerais no site:
    Após um longo perído voltei a mexer no site;
    Inserir sistema de rotas usando express;
    Frontend ainda está puro.

---
+++ js/contato.js
@@ -0,0 +1,48 @@
+document.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    // Verifica se o formulário enviado é o de contato
+    if (form.id === 'contact-form') {
+    }
+
+    let name = document.getElementById('name');
+    let email = document.getElementById('email');
+    let message = document.getElementById('message');
+
+    var data = {
+        name: name.value,
+        email: email.value,
+        message: message.value
+    }
+
+    try {
+        const response = await fetch('https://backend-site-98a38bb92184.herokuapp.com/send-email', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        })
+
+        console.log('Success: ', response.statusText);
+
+        // Exibe a notificação
+        const notificacao = document.getElementById('notificacao');
+        notificacao.style.display = 'block';
+
+        // Aguarda 3 segundos e oculta a notificação
+        setTimeout(() => {
+            notificacao.style.display = 'none';
+        }, 3000);
+
+        // Limpa os campo do form
+        name.value = "";
+        email.value = "";
+        message.value = "";
+
+    } catch (error) {
+        console.error('Error: ', error);
+    }
+
+})
+
--- js/script.js
+++ js/script.js
@@ -1,46 +1,83 @@
-document.getElementById('contact-form').addEventListener('submit', async (e) => {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
 
-    var data = {
-        name: document.getElementById('nome').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('mensagem').value
+    const navigateTo = (url) => {
+        history.pushState(null, null, url);
+        router();
+    };
+
+    const router = async () => {
+
+        const routes = [
+            { path: "/", view: () => "pages/home.html" },
+            { path: "/sobre", view: () => "pages/sobre.html" },
+            { path: "/projetos", view: () => "pages/projetos.html" },
+            { path: "/contato", view: () => "pages/contato.html" },
+            { path: "/blog", view: () => "blog/index.html" }
+        ];
+
+        // Verifica a rota atual
+        const potentialMatches = routes.map(route => {
+            return {
+                route: route,
+                isMatch: location.pathname === route.path
+            };
+        });
+
+
+        // Verifica se há uma correspondência exata
+        let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+        // Se não houver correspondência exata, define a rota padrão
+        if (!match) {
+            match = {
+                route: routes[0],
+                isMatch: true
+            };
+        }
+
+        // Carrega a visualização correspondente
+        try {
+            const html = await fetch(match.route.view()).then(res => {
+                if (!res.ok) {
+                    throw new Error(`HTTP error! status: ${res.status}`);
+                }
+                return res.text();
+            });
+            document.getElementById("app").innerHTML = html;
+        } catch (error) {
+            console.error("Error loading view:", error);
+            document.getElementById("app").innerHTML = "<h1>404</h1><p>Página não encontrada</p>";
+        }
+    };
+
+
+    const links = document.querySelectorAll('a[data-link]');
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const url = link.getAttribute('data-link');
+            removeClasse(url)
+            navigateTo(url)
+        });
+    });
+
+    const removeClasse = (url) => {
+        // console.log(url)
+        if (url !== "/") {
+            const firstLink = document.querySelector('#footer .footer-container-list:nth-child(1) a');
+            if (firstLink) {
+                firstLink.style.filter = 'contrast(30%)';
+                firstLink.style.fontWeight = '300';
+            }
+        } else {
+            const firstLink = document.querySelector('#footer .footer-container-list:nth-child(1) a');
+            if (firstLink) {
+                firstLink.style.filter = 'none';
+                firstLink.style.fontWeight = 'bold';
+            }
+        }
     }
-
-    try {
-        const response = await fetch('https://backend-site-98a38bb92184.herokuapp.com/send-email', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-
-        // const responseData = await response.json();
-        console.log('Success: ', response.statusText);
-
-        // Exibe a notificação
-        const notificacao = document.getElementById('notificacao');
-        notificacao.style.display = 'block';
-
-        // Aguarda 3 segundos e oculta a notificação
-        setTimeout(() => {
-            notificacao.style.display = 'none';
-        }, 3000);
-
-        document.getElementById('nome').value = "";
-        document.getElementById('email').value = "";
-        document.getElementById('mensagem').value = "";
-
-    } catch (error) {
-        console.error('Error: ', error);
-    }
-
-})
-
-function toggleMenu() {
-    const menu = document.getElementById('navbar');
-    if (menu.classList.contains('expandido')) {
-        menu.classList.remove('expandido');
-    } else {
-        menu.classList.add('expandido');
-    }
-}
+    // console.log(window.location.pathname)
+    removeClasse(window.location.pathname);
+    router();
+});
